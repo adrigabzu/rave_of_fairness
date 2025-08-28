1601631b08bb702e7779567207a704b262a21aa6
Adrian G. Zucco <adrigabzu@gmail.com>
2025-08-28 10:50:27 +0200

    Reduced parameters

--- src/parse_data.py
+++ src/parse_data.py
@@ -4,16 +4,18 @@ import pandas as pd
 import os
 
 os.getcwd() 
-
-# %%
-
 # Read the CSV file
 data = pd.read_csv("../data/raw_data/ranking_minorities.csv")
 
 # %%
 
 # Filter rows where k equals 10
-filtered_data = data[data['k'] == 10]
+filtered_data = data[
+    (data['k'] == 10) &
+    (data['h_mm'].isin([0.2, 0.5, 0.8])) &
+    (data['h_MM'].isin([0.2, 0.5, 0.8])) &
+    (data['fm'].isin([0.1, 0.3]))
+]
 
 # Save the filtered data to CSV
 filtered_data.to_csv("../data/processed_data/parameters.csv", index=False)
